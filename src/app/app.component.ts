920906924433bf696502648300a0a1031cfadec9
gabriel12QK <jose.rivas@espam.edu.ec>
2022-06-06 10:12:05 -0500

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'pract_form';
+  public form!:FormGroup;
+   constructor(private _formB:FormBuilder){}
+   ngOnInit(){
+     this.form=this._formB.group( {
+       email:['',  [Validators.required, Validators.email] ],
+       pass:['', [Validators.required, Validators.minLength(6),Validators.pattern(/^[a-z]/)]  ]
+     });
+
+   }
+   send(){
+     console.log(this.form.value)
+   }
 }
+
+
